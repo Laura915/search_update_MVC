1e7db66aa86239f688626f30bfe3e140046eaaa7
Laura <lhernan53112@gmail.com>
2021-02-04 16:22:52 -0500

    Added sprinboot web boiler plate code

---
+++ src/main/java/com/hcl/search_update/SearchUpdateApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.search_update;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchUpdateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchUpdateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcl/search_update/SearchUpdateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.search_update;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchUpdateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
